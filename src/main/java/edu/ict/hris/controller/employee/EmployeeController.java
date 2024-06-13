27e1e467d5262f3ae815aeb4c146e11b1b67f7b9
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-06-13 21:30:05 +0530

    add EmployeeController

---
+++ src/main/java/edu/ict/hris/controller/employee/EmployeeController.java
@@ -0,0 +1,18 @@
+package edu.ict.hris.controller.employee;
+
+import edu.ict.hris.dto.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/emp-controller")
+public class EmployeeController {
+    List<Employee> employeeList;
+
+    @PostMapping("employee")
+    public void add(@RequestBody Employee employee){
+       employeeList.add(employee);
+    }
+
+}
